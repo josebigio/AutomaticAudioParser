f279be5db8ee21fabfb0972e29a89a0f1f90c0f6
jbbigio <jbbigio@audible.com>
2019-01-12 15:50:54 -0500

    able to take a folder from s3 and create output.json

--- src/aws_utils/index.js
+++ src/aws_utils/index.js
@@ -1,73 +1,89 @@
-// import AWS from 'aws-sdk';
-// import {RateLimiter} from 'limiter';
-// import fs from 'fs';
+import AWS from 'aws-sdk';
+import {RateLimiter} from 'limiter';
+import fs from 'fs';
 
-// const FILE_LENGTH = 3600
-// const BUCKET_NAME = "piyin.corp";
-// const BOOK_FOLDER_NAME = "social leap";
-// const PREFIX_NAME = "SOCIAL_LEAP_LARGE"
-// const URL_PREFIX = `https://s3.us-east-2.amazonaws.com/${BUCKET_NAME}/${BOOK_FOLDER_NAME.replace(" ", "+")}/`;
-// const s3Service = new AWS.S3();
-// const transcribeService = new AWS.TranscribeService({ region: 'us-east-2' });
-// const limiter = new RateLimiter(1, 10000); // at most 1 request every 100 ms
+const FILE_LENGTH = 3600
+const BUCKET_NAME = "piyin.corp";
+const BOOK_FOLDER_NAME = "social leap";
+const PREFIX_NAME = "SOCIAL_LEAP_LARGE"
+const URL_PREFIX = `https://s3.us-east-2.amazonaws.com/${BUCKET_NAME}/${BOOK_FOLDER_NAME.replace(" ", "+")}/`;
+const s3Service = new AWS.S3();
+const transcribeService = new AWS.TranscribeService({ region: 'us-east-2' });
+const limiter = new RateLimiter(1, 10000); // at most 1 request every 100 ms
 
-// interface BookPart {
-//   url: string,
-//   fileName: string
-// }
+export function getAllFiles(prefix) {
+  return s3Service
+    .listObjects({
+      Delimiter: "/",
+      Prefix: prefix,
+      Bucket: "piyin.corp"
+    }).promise()
+    .then((response) => {
+      if (response && response.Contents) {
+        return response.Contents
+          .filter(fileObject => {
+            return fileObject.Size && fileObject.Size > 0 && fileObject.Key
+          })
+      }
+      return []
+    })
+}
 
-// function getAllFiles(prefix: string): Promise<AWS.S3.Types.ObjectList> {
-//   return s3Service
-//     .listObjects({
-//       Delimiter: "/",
-//       Prefix: prefix,
-//       Bucket: "piyin.corp"
-//     }).promise()
-//     .then((response) => {
-//       if (response && response.Contents) {
-//         return response.Contents
-//           .filter(fileObject => {
-//             return fileObject.Size && fileObject.Size > 0 && fileObject.Key
-//           })
-//       }
-//       return []
-//     })
-// }
+export function getAllFileNames(prefix) {
+  return getAllFiles(prefix)
+        .then((response)=>{
+          return response.map((fileObject)=>fileObject.Key)
+        })
+}
+
+export function getJSONOutputFromS3File(fileName) {
+    return s3Service.getObject({Bucket: "piyin.corp", Key: fileName}).promise()
+    .then(response => {
+      return JSON.parse(response.Body.toString('utf-8'))
+    })
+
+}
+
+const sort = (key1, key2) => {
+  key1 > key2
+  // return Number(key1.replace(PREFIX_NAME,"").replace("-","").replace(".json","")) - 
+  // Number(key2.replace(PREFIX_NAME,"").replace("-","").replace(".json",""))
+}
 
 
-// function getAllBooks(prefix: string): Promise<Array<BookPart>> {
-//  return getAllFiles(prefix)
-//     .then((response) => {
-//         return response
-//           .map((fileObject) => {
-//             const fileName = fileObject.Key!.replace(BOOK_FOLDER_NAME, "").replace("/", "")
-//             const result: BookPart = {
-//               url: URL_PREFIX + fileName,
-//               fileName: fileName
-//             }
-//             return result;
-//           });
-//     })
-// }
+function getAllBooks(prefix) {
+ return getAllFiles(prefix)
+    .then((response) => {
+        return response
+          .map((fileObject) => {
+            const fileName = fileObject.Key.replace(BOOK_FOLDER_NAME, "").replace("/", "")
+            const result = {
+              url: URL_PREFIX + fileName,
+              fileName: fileName
+            }
+            return result;
+          });
+    })
+}
 
-// // getAllBooks("social leap/")
-// //   .then(response => {
-// //     for (let i = 10; i < response.length; i++) {
-// //       limiter.removeTokens(1,()=>{
-// //         const bookPart = response[i]
-// //         const params = makeTranscribeParams(`${PREFIX_NAME}-${i}`, bookPart.url)
-// //         transcribeService.startTranscriptionJob(params).promise().then((response)=>{
-// //           console.log("TRANSCRIBE RESPONSE: ", response)
-// //         })
-// //       })
-// //     }
-// //   })
+// getAllBooks("social leap/")
+//   .then(response => {
+//     for (let i = 10; i < response.length; i++) {
+//       limiter.removeTokens(1,()=>{
+//         const bookPart = response[i]
+//         const params = makeTranscribeParams(`${PREFIX_NAME}-${i}`, bookPart.url)
+//         transcribeService.startTranscriptionJob(params).promise().then((response)=>{
+//           console.log("TRANSCRIBE RESPONSE: ", response)
+//         })
+//       })
+//     }
+//   })
 
 // const promiseList = getAllFiles("")
 //   .then(response=>{
 //     return response.filter((fileObject)=>{
-//       return fileObject.Key!.includes(PREFIX_NAME)
-//     }).map(responseObject=>responseObject.Key!)
+//       return fileObject.Key.includes(PREFIX_NAME)
+//     }).map(responseObject=>responseObject.Key)
 //     .sort((key1, key2)=>{
 //       return Number(key1.replace(PREFIX_NAME,"").replace("-","").replace(".json",""))
 //      - Number(key2.replace(PREFIX_NAME,"").replace("-","").replace(".json",""))
@@ -81,14 +97,14 @@
 //   promiseList.then((promiseList)=>{
 //     return Promise.all(promiseList).then( result => {
 //       return result.map((s3Object, index)=>{
-//         const jsonString =  s3Object.Body!.toString()
+//         const jsonString =  s3Object.Body.toString()
 //         const json = JSON.parse(jsonString);
 //         return makeArray(json.results.items, index*FILE_LENGTH)
 //       })
 //     })
 //   })
 //   .then(listOfWords => {
-//     var wordList: any[] = []
+//     var wordList = []
 //     for(const miniWordList of listOfWords) {
 //       wordList = wordList.concat(miniWordList);
 //     }
@@ -96,51 +112,51 @@
 
 //   })
 
-//   const makeArray = (items: any, offset: number)=>{
-//     const result = []
-//     for(let item of items) {
-//         const word = item.alternatives[0].content;
-//         if( item.start_time ) {
-//             result.push(
-//                 {
-//                 "word": item.alternatives[0].content,
-//                 "startTime": "" + (Number(item.start_time) + offset).toFixed(3),
-//                 "endTime": "" + (Number(item.end_time) + offset).toFixed(3)
-//                 }
-//             )
-//         }
-//         else {
-//             result[result.length-1].word = result[result.length-1].word + word
-//         }
-//     }
-//     return result;
-//   }
+  const makeArray = (items, offset)=>{
+    const result = []
+    for(let item of items) {
+        const word = item.alternatives[0].content;
+        if( item.start_time ) {
+            result.push(
+                {
+                "word": item.alternatives[0].content,
+                "startTime": "" + (Number(item.start_time) + offset).toFixed(3),
+                "endTime": "" + (Number(item.end_time) + offset).toFixed(3)
+                }
+            )
+        }
+        else {
+            result[result.length-1].word = result[result.length-1].word + word
+        }
+    }
+    return result;
+  }
 
 
   
-//   // .then(
-//   //   response=>{
-//   //     return  s3Service.getObject({Bucket: "piyin.corp", Key: key}).promise();
-//   //     for(const key of response) {
-//   //       s3Service.getObject({Bucket: "piyin.corp", Key: key}).promise().then(object=>{
-//   //         const jsonString =  object.Body!.toString()
-//   //         const json = JSON.parse(jsonString);
-//   //         const items = json.results.items
-//   //         console.log(resultList);
-//   //       })
-//   //     }
-//   //   }
-//     // s3Service.getObject(
-//     //   { Bucket: "my-bucket", Key: "my-picture.jpg" },
-//     //   function (error, data) {
-//     //     if (error != null) {
-//     //       alert("Failed to retrieve an object: " + error);
-//     //     } else {
-//     //       alert("Loaded " + data.ContentLength + " bytes");
-//     //       // do something with data.Body
-//     //     }
-//     //   }
-//     // );
+  // .then(
+  //   response=>{
+  //     return  s3Service.getObject({Bucket: "piyin.corp", Key: key}).promise();
+  //     for(const key of response) {
+  //       s3Service.getObject({Bucket: "piyin.corp", Key: key}).promise().then(object=>{
+  //         const jsonString =  object.Body!.toString()
+  //         const json = JSON.parse(jsonString);
+  //         const items = json.results.items
+  //         console.log(resultList);
+  //       })
+  //     }
+  //   }
+    // s3Service.getObject(
+    //   { Bucket: "my-bucket", Key: "my-picture.jpg" },
+    //   function (error, data) {
+    //     if (error != null) {
+    //       alert("Failed to retrieve an object: " + error);
+    //     } else {
+    //       alert("Loaded " + data.ContentLength + " bytes");
+    //       // do something with data.Body
+    //     }
+    //   }
+    // );
 
 
 
@@ -150,15 +166,15 @@
 
 
 
-// const makeTranscribeParams = function (jobName: string, url: string): AWS.TranscribeService.StartTranscriptionJobRequest {
-//   return {
-//     LanguageCode: "en-US",
-//     Media: {
-//       MediaFileUri: url
-//     },
-//     MediaFormat: "mp3",
-//     TranscriptionJobName: jobName,
-//     OutputBucketName: BUCKET_NAME
-//   }
+const makeTranscribeParams = function (jobName, url) {
+  return {
+    LanguageCode: "en-US",
+    Media: {
+      MediaFileUri: url
+    },
+    MediaFormat: "mp3",
+    TranscriptionJobName: jobName,
+    OutputBucketName: BUCKET_NAME
+  }
 
-// };
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import fs from "fs"
+import { getAllFileNames, getJSONOutputFromS3File } from './aws_utils'
+import { range } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+import { Observable } from "rxjs/Observable";
+
 
 const FILE_DIR = "./fear_and_loathing";
 const LENGHT_OF_FILE = 3600
+const AWS_PREFIX = "fear and loathing in las vegas/transcription/"
 
-fs.readdir(FILE_DIR, function(err, items) {
-    let result = []
-    for (var i=0; i<items.length; i++) {
-        const fileItems = getItemsFromFile(FILE_DIR + "/" + items[i])
-        const parsedArray = getParsedArray(fileItems, i*LENGHT_OF_FILE)
+getAllFileNames(AWS_PREFIX).then(listOfFilesNames => {
+    const fileNamesPromises = listOfFilesNames.map((fileName) => {
+        return getJSONOutputFromS3File(fileName)
+    });
+    return Promise.all(fileNamesPromises);
+}).then(fileJsons => {
+    console.log("got responses")
+    var result = []
+    for (var i = 0; i < fileJsons.length; i++) {
+        const items = fileJsons[i].results.items
+        const parsedArray = getParsedArray(items, i * LENGHT_OF_FILE)
         result = result.concat(parsedArray)
     }
     fs.writeFileSync('output.json',JSON.stringify({"words":result}),'utf8');
 });
+
+// getAllFileNames(AWS_PREFIX).then(result => {
+//    return result[0]
+// }).then(fileName => {
+//     return getJSONOutputFromS3File(fileName)
+// })
+// .then(output => {
+//     console.log(output)
+// })
+// fs.readdir(FILE_DIR, function(err, items) {
+//     let result = []
+//     for (var i=0; i<items.length; i++) {
+//         const fileItems = getItemsFromFile(FILE_DIR + "/" + items[i])
+//         const parsedArray = getParsedArray(fileItems, i*LENGHT_OF_FILE)
+//         result = result.concat(parsedArray)
+//     }
+//     fs.writeFileSync('output.json',JSON.stringify({"words":result}),'utf8');
+// });
 
 const getItemsFromFile = (filePath) => {
     const json = JSON.parse(fs.readFileSync(filePath, 'utf8'))
@@ -21,19 +51,19 @@ const getItemsFromFile = (filePath) => {
 
 const getParsedArray = (items, offset) => {
     const result = []
-    for(let item of items) {
+    for (let item of items) {
         const word = item.alternatives[0].content;
-        if( item.start_time ) {
+        if (item.start_time) {
             result.push(
                 {
-                "word": item.alternatives[0].content,
-                "startTime": parseFloat(item.start_time) + offset + "",
-                "endTime": parseFloat(item.end_time) + offset + ""
+                    "word": item.alternatives[0].content,
+                    "startTime": parseFloat(item.start_time) + offset + "",
+                    "endTime": parseFloat(item.end_time) + offset + ""
                 }
             )
         }
         else {
-            result[result.length-1].word = result[result.length-1].word + word
+            result[result.length - 1].word = result[result.length - 1].word + word
         }
     }
     return result
